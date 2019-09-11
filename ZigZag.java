be8c8a1a02425b3424ccb34163551331f2e78ce6
xrshushu <54711220+xrshushu@users.noreply.github.com>
2019-09-11 23:26:28 +0800

    006. Z 字形变换
    题目转自力扣：https://leetcode-cn.com/problems/zigzag-conversion/submissions/

---
+++ ZigZag.java
@@ -0,0 +1,25 @@
+public ZigZag {
+    public static String convert(String s, int numRows) {
+        if(numRows == 1)
+            return s;
+        
+        int len = Math.min(s.length(), numRows);
+        String []rows = new String[len];		//对应每一行
+        for(int i = 0; i< len; i++) rows[i] = "";		//初始化
+        int loc = 0;
+        boolean down = false;
+
+        for(int i=0;i<s.length();i++) {
+            rows[loc] += s.substring(i,i+1);
+            if(loc == 0 || loc == numRows - 1)
+                down = !down;
+            loc += down ? 1 : -1;
+        }
+        
+        String ans = "";
+        for(String row : rows) {
+            ans += row;
+        }
+        return ans;
+    }
+}
